73c38fc06dfa0ed8835cff0b69418aaa437d6858
kobadze888 <mr.kobadze@gmail.com>
2025-12-16 22:15:29 +0400

    fix-favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,15 +4,18 @@ export default function Document() {
   return (
     <Html lang="ru">
       <Head>
-        {/* Favicon - SVG (თანამედროვე ბრაუზერებისთვის) */}
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        {/* 1. SVG - მთავარი ხატულა (სკალირებადი, საუკეთესოა ხარისხისთვის) */}
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any" />
         
-        {/* Favicon - აი აქ ვამატებთ */}
-        <link rel="icon" href="/favicon.png" sizes="any" />
+        {/* 2. PNG - სარეზერვო (Google-ისთვის და ძველი ბრაუზერებისთვის) */}
+        <link rel="icon" href="/favicon.png" type="image/png" />
         
-        {/* Google Font: Inter */}
+        {/* 3. Apple Devices - აუცილებელია iOS-ისთვის (Home Screen-ზე დამატებისას) */}
+        <link rel="apple-touch-icon" href="/favicon.png" />
+        
+        {/* Google Fonts - შესწორებული crossOrigin */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </Head>
       <body className="bg-[#10141A] text-white font-sans">
         <Main />
